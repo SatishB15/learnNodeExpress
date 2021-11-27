eb08b641818c1d3a588446eb3ee754047b5cbefb
SatishB15 <satishbirhade16@gmail.com>
2021-11-27 22:38:48 +0530

    FIles Added

--- Web Frameworks/practical-5.js
+++ Web Frameworks/practical-5.js
@@ -3,14 +3,11 @@ const http = require('http');
 const server = http.createServer(function (req, res)
 {
 res.writeHead(200,{"Content-Type":"text/html"});
-res.write("Server Started....!");
-res.write('<form enctype=multipart/>')
-
-
-
+res.write('<form><input type="file" name="" id="file"></form>')
 res.end();
 });
 
 server.listen(8000);
 
 console.log('Node.js web server at port 8000 is running..');
+
