a1f59a35b50657fab7b0447ca197917bc699fed3
Satish_15 <53379230+SatishB15@users.noreply.github.com>
2021-12-15 08:30:08 +0530

    Add files via upload

---
+++ Web Frameworks/practical-13.js
@@ -0,0 +1,58 @@
+// npm install mysql express express-session body-parser path --save
+
+let mysql = require('mysql');
+let express = require('express');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+let path = require('path');
+
+let connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'satish',
+	database : 'UserLogin'
+});
+
+let app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	let username = request.body.username;
+	let password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+console.log("server is runing on port- 5000")
+app.listen(5000);
